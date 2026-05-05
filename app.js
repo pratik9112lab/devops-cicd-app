78956760c345387c4677bc4d5a8384f53c086bf4
pratik <pratik@gmail.com>
2026-05-06 03:28:57 +0530

    connect app to postgres

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const { Client } = require('pg');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Working 🚀');
+const client = new Client({
+  host: 'db',            // IMPORTANT (service name from docker-compose)
+  user: 'user1',
+  password: 'welcome',
+  database: 'mydb',
+  port: 5432
+});
+
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL ✅'))
+  .catch(err => console.error('DB connection error:', err));
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await client.query('SELECT NOW()');
+    res.send(`DB Time: ${result.rows[0].now}`);
+  } catch (err) {
+    res.send('Error connecting DB');
+  }
 });
 
 app.listen(3000, () => {

0bddb52564b557d1a6cd5f47c317ff7027cc2435
pratik <pratik@gmail.com>
2026-05-07 04:56:56 +0530

    modify app.js

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
 const express = require('express');
-const { Client } = require('pg');
 
 const app = express();
 
-const client = new Client({
-  host: 'db',
-  user: 'user1',
-  password: 'welcome',
-  database: 'mydb',
-  port: 5432
-});
-
-const connectDB = async () => {
-  try {
-    await client.connect();
-    console.log('Connected to PostgreSQL ✅');
-  } catch (err) {
-    console.log('DB not ready, retrying in 5s...');
-    setTimeout(connectDB, 5000);
-  }
-};
-
-connectDB();
-
-app.get('/', async (req, res) => {
-  try {
-    const result = await client.query('SELECT NOW()');
-    res.send(`DB Time: ${result.rows[0].now}`);
-  } catch (err) {
-    console.error(err);
-    res.send('DB ERROR');
-  }
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>🚀 CI/CD Pipeline Working!</h1>
+    <p>Docker + GitHub Actions + Self Hosted Runner</p>
+    <p>Deployment successful ✅</p>
+  `);
 });
 
 app.listen(3000, '0.0.0.0', () => {
